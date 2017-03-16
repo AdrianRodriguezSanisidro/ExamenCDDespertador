406df64e1e158d069eac4d82b837f4c69c351ae4
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-03-16 09:14:52 +0100

    Getters and setters added

--- src/examencddespertador/Reloj.java
+++ src/examencddespertador/Reloj.java
@@ -10,5 +10,42 @@ package examencddespertador;
  * @author Adry
  */
 public class Reloj {
+    private boolean modoHora=true;
+    private int horas;
+    private int minutos;
+    private boolean alarmaActiva=false;
+
+    public boolean isModoHora() {
+        return modoHora;
+    }
+
+    public void setModoHora(boolean modoHora) {
+        this.modoHora = modoHora;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public int getMinutos() {
+        return minutos;
+    }
+
+    public void setMinutos(int minutos) {
+        this.minutos = minutos;
+    }
+
+    public boolean isAlarmaActiva() {
+        return alarmaActiva;
+    }
+
+    public void setAlarmaActiva(boolean alarmaActiva) {
+        this.alarmaActiva = alarmaActiva;
+    }
     
-}
+    }
+
