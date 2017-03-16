cec4d7c6a62700cfc7284aacd16537381fc3f425
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-03-16 09:15:10 +0100

    switch case created

--- src/examencddespertador/ExamenCDDespertador.java
+++ src/examencddespertador/ExamenCDDespertador.java
@@ -3,6 +3,8 @@
  */
 package examencddespertador;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Adry
@@ -13,7 +15,12 @@ public class ExamenCDDespertador {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-    }
+        int eleccion=5;
+        do{
+        switch(eleccion=Integer.parseInt(JOptionPane.showInputDialog("1=Cambiar modo;2=Aumentar las horas;3=Aumentar los minutos;4=Activar/Desactivar la alarma"))){
+            case 1:
+        }
+    }while(eleccion>4&&eleccion<1);
     
+}
 }
