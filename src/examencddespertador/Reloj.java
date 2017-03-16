7a6207ac52e17f6fcf37b648affcc2b8e5dde315
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-03-16 09:54:44 +0100

    Coments added

--- src/examencddespertador/Reloj.java
+++ src/examencddespertador/Reloj.java
@@ -14,47 +14,86 @@ public class Reloj {
     private int horas;
     private int minutos;
     private boolean alarmaActiva=false;
-
+/**
+ * @return modoHora
+ */
     public boolean isModoHora() {
         return modoHora;
     }
-
+/**
+ * 
+ * @param modoHora 
+ */
     public void setModoHora(boolean modoHora) {
         this.modoHora = modoHora;
     }
-
+/**
+ * 
+ * @return horas
+ */
     public int getHoras() {
         return horas;
     }
-
+/**
+ * 
+ * @param horas 
+ */
     public void setHoras(int horas) {
         this.horas = horas;
     }
-
+/**
+ * 
+ * @return minutos
+ */
     public int getMinutos() {
         return minutos;
     }
-
+/**
+ * 
+ * @param minutos 
+ */
     public void setMinutos(int minutos) {
         this.minutos = minutos;
     }
-
+/**
+ * 
+ * @return alarmaActiva
+ */
     public boolean isAlarmaActiva() {
         return alarmaActiva;
     }
-
+/**
+ * 
+ * @param alarmaActiva 
+ */
     public void setAlarmaActiva(boolean alarmaActiva) {
         this.alarmaActiva = alarmaActiva;
     }
+/**
+ * Change the alrm mode
+ * @return modoHora
+ */
     public boolean cambiarModo(){
         return modoHora;
     }
+/**
+ * The heurs grow up 1 heur
+ * @return horas
+ */
     public int aumentarHoras(){
         return horas;
     }
+    /**
+     * The minutes grow up 1 minute
+     * @return minutos
+     */
     public int aumentarMinutos(){
         return minutos;
     }
+    /**
+     * Active or desactive the alarm
+     * @return alarmaActiva
+     */
     public boolean activarAlarma(){
         return alarmaActiva;
     }
