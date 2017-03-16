7e445c09e6f097fdfcdca641b88b1f84e55a76ad
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-03-16 09:56:54 +0100

    Coments added

--- src/examencddespertador/Alarma.java
+++ src/examencddespertador/Alarma.java
@@ -12,25 +12,47 @@ package examencddespertador;
 public class Alarma {
     private int horaAlarma;
     private int minAlarma;
-
+/**
+ * 
+ * @return horaAlarma
+ */
     public int getHoraAlarma() {
         return horaAlarma;
     }
-
+/**
+ * 
+ * @param horaAlarma 
+ */
     public void setHoraAlarma(int horaAlarma) {
         this.horaAlarma = horaAlarma;
     }
-
+/**
+ * 
+ * @return minAlarma
+ */
     public int getMinAlarma() {
         return minAlarma;
     }
-
+/**
+ * 
+ * @param minAlarma 
+ */
     public void setMinAlarma(int minAlarma) {
         this.minAlarma = minAlarma;
     }
+/**
+ * Get the heurs and set the alarm with these heurs
+ * @param horas
+ * @return horaAlarma
+ */
     public int ponerHoraAlarma(int horas){
         return horaAlarma;
     }
+/**
+ * Get the minutes and ser the alarm with these minutes
+ * @param minutos
+ * @return minAlarma
+ */
     public int ponerMinAlarma(int minutos){
         return minAlarma;
     }
